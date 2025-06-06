1c7c28bb1ed95f4d356360ff7be99f7c4e16299f
rhine399 <uwaara@gmail.com>
2025-06-06 10:58:44 -0500

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,10 @@
+print("hello world")
+#this is my 1st python code
+print(7+9)
+name="Rhine"
+hobby="Coding"
+print("my name is", name)
+print(f"my name is {name}and my hobby is {hobby}")
+print("keywords in python...")
+import keyword
+print(keyword.kwlist)
